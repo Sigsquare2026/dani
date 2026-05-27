2929d3fa136e685f0030a20daf8f36f481593e86
Sigsquare2026 <sigsquare2026@gmail.com>
2026-05-28 06:38:01 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,1051 @@
 const videos = [
   {
     category: "엑셀시그",
-    number: "1212",
-    title: "진압해",
-    thumbnail: "thumbnails/엑셀시그/1212.gif",
+    number: "1000",
+    title: "모찌송",
+    thumbnail: "https://sig.cnine.kr/signature/20260503/0f50b76a732e4bf68c03a70b7068442f.png",
 
-    video: "https://pub-xxxx.r2.dev/엑셀시그/1212/진압해.mp4",
+    video: "공란",
 
     clips: [
       {
-        label: "2026.05.26",
-        video: "https://pub-xxxx.r2.dev/엑셀시그/1212/진압해.mp4"
+        label: "2026.06.20",
+        video: "공란"
       },
       {
-        label: "2026.05.05",
-        video: "https://pub-xxxx.r2.dev/엑셀시그/1212/진압해-2.mp4"
+        label: "2026.06.20",
+        video: "공란"
       }
     ]
   },
+
+{
+  category: "엑셀시그",
+  number: "1077",
+  title: "츄",
+  thumbnail: "https://sig.cnine.kr/signature/20260503/11a92c4d8d004860a28016621dcededc.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1212",
+  title: "진압해",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/b98f7776b0d44dcb90aebb44dbeb8f50.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1222",
+  title: "두번째지구",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/a0286328140b4acdb3f5cdaa23d5c2a1.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1225",
+  title: "첫눈",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/a16bdbf12c86450aac2f7a1e55e2319c.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1234",
+  title: "왜요왜요",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/aff01c2b84a4427b82d837aa0ece7298.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1240",
+  title: "Taylor Swift",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/f9b9113385c94668b95c1e81dbe27985.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1245",
+  title: "싸이코",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/c37affa4615f4c81b304692c302e8fe6.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1252",
+  title: "날라리",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/debc7ac3a3434f83bdebfd2983f19e6e.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1272",
+  title: "카리나 닮았대",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/9f294f65b7cf4a96a1da47a43f0ff6a2.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1279",
+  title: "Candy Theif",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/f087e84501c14a3fba08ccfebd2478a0.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1280",
+  title: "너만 아니면 돼",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/d85aefe6657b487d8a6f8cdf7fb89901.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1288",
+  title: "버블버블",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/4b9456c0b5264b5995ae0bb374c4704a.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1323",
+  title: "쓸애기",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/622f1db7c74e4f05aa7d4e1dbb666cb4.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1329",
+  title: "쌔끈빠끈",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/c792d7e62eda4a1c831dc1f7eb22c5cf.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1332",
+  title: "마그네틱",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/8418d3e0bcd24c24a57588052bb54f70.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1333",
+  title: "무아",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/dd7132e8c0a5408382978934347576e4.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1345",
+  title: "Body",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/651774648fa641c6b6cdc4afe6a09e80.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1444",
+  title: "bloadline",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/e322880fc1b74ea69718605cdc6114fd.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1445",
+  title: "힙합보단사랑",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/d07655380ad343f1ab971e87db3e0e31.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1452",
+  title: "빛을따라서",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/38b85be3f91146ea8c708f08cd3d6a11.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1500",
+  title: "바빠",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/8f1fbd17068e438ebb1a794b354569ad.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1510",
+  title: "Over Drive",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/ccc265bad4e54855a9f4a32673ec2b2d.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1520",
+  title: "외모체크",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/5b10de9c39ed48ffbe73611a28c311f2.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1521",
+  title: "러브쉐이크",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/f89fafa326d448e1ab708761c59004a5.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1528",
+  title: "찔려",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/0a089b0f87bf4a66ae50e63419793f95.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1547",
+  title: "샤넬챌린지",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/ece5c36587b1402cbb5c8bc831ac3974.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1572",
+  title: "키딩",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/d8f694b0eb654cb7b37093946ad30364.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1627",
+  title: "DUET",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/1fcc5c2dc8574d50bf90223453929805.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1666",
+  title: "It's Sexy",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/bca3c5b5642e4782ba5949c790d5315a.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1676",
+  title: "모시모시",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/5d20960338d846348a88f1d018faef03.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1678",
+  title: "여름이었다",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/874d9d716935484b8f3fedb067f21cb2.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1717",
+  title: "불도저",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/e794b6918970420fa786061186c6cf68.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1771",
+  title: "매끈매끈",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/dcd57363f1c34526b715a61fdf2b570b.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1804",
+  title: "New ERA",
+  thumbnail: "https://sig.cnine.kr/signature/20260329/13366b06c32449a3a0b5367d9c3503ab.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1818",
+  title: "fucking banger",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/a3236911093141448543e1bb371b4653.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1819",
+  title: "WTF",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/8a17ff7493f44514b95e5e4e434da4d2.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1828",
+  title: "킹받쥬",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/58ddb2aae99f4e89a320feec7ec41ba6.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1842",
+  title: "씨봐레츠코",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/9bf739854e3a498481c638180287acea.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1852",
+  title: "RUDE",
+  thumbnail: "https://sig.cnine.kr/signature/20260329/e80aeab69ac84fde8e6857d4ea0b3112.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1878",
+  title: "캐치캐치",
+  thumbnail: "https://sig.cnine.kr/signature/20260329/2cc5e879885f477cb0da6c6dbdff9ea2.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1881",
+  title: "피리송",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/297fb81cd3dd4897ac05d66b2ece11ce.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1884",
+  title: "야메떼",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/ed04781eafa24c08a157a4309a2c61aa.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1888",
+  title: "롤러코스터",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/f44c4c2f9825410f941a5f87a5ffe74f.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1919",
+  title: "간바레센빠이",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/1fa5677217ef41e0a22e7b5d45078603.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "1997",
+  title: "런웨이",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/9eb23fb9a6e84b65b07151c0a600efe2.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2000",
+  title: "팬서비스",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/2f8e44cfdf4643198312b67328dc15f9.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2001",
+  title: "붐샤카라카",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/87ed61aadc084ab98578f18d5003db2a.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2002",
+  title: "https://sig.cnine.kr/signature/20260328/183f739a2fdb4e4692d6cfe263ef215d.png",
+  thumbnail: "공란",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2018",
+  title: "Whiplash",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/3b4da5eef05048e4921475ad64c9b220.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2019",
+  title: "몸",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/1c427315cdc44f76b21fe2aaf7f87579.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2020",
+  title: "Bass down low",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/5839203077204b369c5e579263b4ab68.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2021",
+  title: "https://sig.cnine.kr/signature/20260328/98682ab2c0204ae1a34bedd6d1ebd81f.png",
+  thumbnail: "공란",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2025",
+  title: "우주로",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/d383170866c94cd688d9f4e6b1392476.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2033",
+  title: "I want you",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/34d8a807f5b749418b9dd95681c3a8e4.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2034",
+  title: "Sexy food",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/00f701902ecd45f29c876eb1fec5d63b.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2225",
+  title: "오늘만 I love you",
+  thumbnail: "https://sig.cnine.kr/signature/20260328/60f78a24765c4814ac3bfd47dd975404.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+{
+  category: "엑셀시그",
+  number: "2288",
+  title: "Bangbang",
+  thumbnail: "https://sig.cnine.kr/signature/20260329/4e29f06c0d6349f4b9558bbbe3933dfe.png",
+
+  video: "공란",
+
+  clips: [
+    {
+      label: "2026.06.20",        video: "공란"
+      },
+      {
+        label: "2026.06.20",
+        video: "공란"
+      }
+    ]
+  },
+
 
   {
     category: "갠방시그",
